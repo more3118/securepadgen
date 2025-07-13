d5e80551cfbeabe5424062e293d30ee983e47dfe
more3118 <more3118@gmail.com>
2025-07-13 13:26:41 -0500

    Created Input Class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Pad Encyption by Shreyas More\nPlease input the secret key:");
+        String key = sc.nextLine();
+
+
+
+        }
+    }
+}
